888d687b1250f6aa8f5f6e60606458cfd1672277
Sasi <sasi@Sasis-MacBook-Pro.local>
2018-08-05 20:03:01 +0530

    Demo changes

---
+++ CustomMap/MainViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MainViewController.swift
+//  CustomMap
+//
+//  Created by Sasi on 8/5/18.
+//  Copyright © 2018 Delta Technology And Management Services Private Limited. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
